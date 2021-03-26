8d89318c86483bc50f7c608751e1c89d78c044db
xiaojin <xiaojin@onemt.com.cn>
2021-03-26 14:20:21 +0800

    ✨feat(debuger): 集成 skynet 调试

--- lualib/skynet.lua
+++ lualib/skynet.lua
@@ -1049,4 +1049,11 @@ debug.init(skynet, {
 	resume = coroutine_resume,
 })
 
+-- vscode debug
+local vscdebug = require "skynet.vscdebug"
+vscdebug.init(skynet, {
+	suspend = suspend,
+	resume = coroutine_resume,
+})
+
 return skynet
